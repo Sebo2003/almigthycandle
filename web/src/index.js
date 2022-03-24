33a7a1ad7c6323b864722232712da6bbd6c395ff
Sebo <GeneralG2003@gmail.com>
2022-03-23 19:03:16 -0500

    Added PredictionsTable component, used for the ai predictions table
    added PredictionsTable to the component folder along with its accompanying css file. I also added a green arrow png, file "greenArrow.png", to the public folder.

--- web/src/index.js
+++ web/src/index.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom';
 // Import our custom React components and any other files.
 import Header from './components/Header/Header';
 import Graph from './components/Graph/Graph';
+import PredictionsTable from './components/PredictionsTable/PredictionsTable';
 import './index.css';
 
 // Render the app components.
@@ -12,6 +13,7 @@ ReactDOM.render(
   <>
     <Header />
     <Graph />
+    <PredictionsTable />
   </>,
   document.querySelector("#root")
 )
